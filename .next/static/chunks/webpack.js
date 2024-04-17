6127d55134b801927fc50f0151393a3af5ba09f1
TenHam3 <simbaandbella3@gmail.com>
2024-04-17 01:06:26 -0500

    Created a register page similar to login and laid out basic skeleton code for creating users

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d45ba3869d137564"; }
+/******/ 		__webpack_require__.h = function() { return "800c404d0865e707"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
