5c72c37b943b8af1b35fff5db99b640a209ed504
Vishnu Patel <viptl123@gmail.com>
2024-04-24 14:36:38 -0500

    modal changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e0d06435db22647c"; }
+/******/ 		__webpack_require__.h = function() { return "0b45f12e9b4220a4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/Components/AddMarkerStylings.js
+++ src/Components/AddMarkerStylings.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
 
 const customStyles = {
   content: {
@@ -9,12 +12,14 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
+    width: 600,
   },
 };
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 
 const AddMarkerStyling = ()=> {
+    const [startDate, setStartDate] = useState(new Date());
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
@@ -34,25 +39,73 @@ const AddMarkerStyling = ()=> {
   return (
     <div>
       <button onClick={openModal}>Open Modal</button>
-      <Modal
+      <Modal 
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Example Modal"
+        id ="modal"
       >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+      <label>Event Name:
+        <input type="text" name="name"/>
+      </label>
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}></h2>
         <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
         <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
+            <label>Location: </label>
+                <select>
+                    <option value="Nielson Hall">Nielson Hall</option>
+                    <option value="Dale Hall">Dale Hall</option>
+                    <option value="Gallogy Hall">Gallogy Hall</option>
+                </select>
+            <label>Date: </label>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            <label>Time of Day: </label>
+            <select> 
+                
+                <option value="AM">AM</option>
+                <option value="PM">PM</option>
+            </select>
+            <label> Hour: </label>
+            <select>
+                
+                <option value ="1">1</option>
+                <option value ="2">2</option>
+                <option value ="3">3</option>
+                <option value ="4">4</option>
+                <option value ="5">5</option>
+                <option value ="6">6</option>
+                <option value ="7">7</option>
+                <option value ="8">8</option>
+                <option value ="9">9</option>
+                <option value ="10">10</option>
+                <option value ="11">1</option>
+                <option value ="12">12</option>
+            </select>
+            <label> Minute: </label>
+            <select> 
+                
+                <option value ="0">0</option>
+                <option value ="5">5</option>
+                <option value ="10">10</option>
+                <option value ="15">15</option>
+                <option value ="20">20</option>
+                <option value ="25">25</option>
+                <option value ="30">30</option>
+                <option value ="35">35</option>
+                <option value ="40">40</option>
+                <option value ="45">45</option>
+                <option value ="50">50</option>
+                <option value ="55">55</option>            
+            </select>
+            <label>Description: 
+                <input type="text" name="description"></input>
+            </label>
         </form>
       </Modal>
     </div>
   );
 }
 export default AddMarkerStyling;
+
