72ad340b61990ae6de151fefc67244ecc8bb6951
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-04-24 13:55:05 -0500

    Installed flaks and made nessary changes.

--- webscraper/app.py
+++ webscraper/app.py
@@ -1,9 +1,15 @@
+from flask import Flask
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from scraper import intialize_scraper
 
 
+flaskApp = Flask(__name__)
+
+
+@flaskApp.route("/")
 def main():
     cred = credentials.Certificate(
         "/Users/tuannguyen/Desktop/ou-mapz/ou-mapz-markers.json"
@@ -17,6 +23,8 @@ def main():
     for i in data:
         db.collection("Engage Data").add(i)
 
+    return "data uploaded"
+
 
 if __name__ == "__main__":
-    main()
+    flaskApp.run(debug=True)
