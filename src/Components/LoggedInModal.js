06147e71acb9bc04447cd460b03990f27006d725
TenHam3 <simbaandbella3@gmail.com>
2024-04-30 14:39:36 -0500

    Styled the modal

--- src/Components/LoggedInModal.js
+++ src/Components/LoggedInModal.js
@@ -14,13 +14,16 @@ const customStyles = {
     transform: 'translate(-50%, -50%)',
     width: 600,
     height: 300,
+    backgroundColor: '#841617',
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'space-between',
   },
 };
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 
 const LoggedInPopup = ()=> {
-  let subtitle;
   const { currentUser } = useAuth()
   const { logout } = useAuth()
   const pageRouter = useRouter()
@@ -28,6 +31,8 @@ const LoggedInPopup = ()=> {
   const heading = useRef();
   const message = useRef();
   const window = useRef();
+  const artificialPadding = useRef();
+  const artificialPaddingTwo = useRef();
 
   function openModal() {
     setIsOpen(true);
@@ -37,13 +42,12 @@ const LoggedInPopup = ()=> {
     // references are now sync'd and can be accessed.
     heading.current.style.color = '#FFFFFF';
     heading.current.style.fontSize = '48px';
-    heading.current.style.backgroundColor = '#841617'; // hex value of red used by OU btw
     heading.current.style.textAlign = 'center';
-    message.current.style.color = '#BCDCEB';
-    message.current.style.backgroundColor = '#841617';
+    message.current.style.color = '#FFFFFF';
     message.current.style.fontSize = '24px';
     message.current.style.textAlign = 'center';
-    window.current.style.backgroundColor = 'black';
+    artificialPadding.current.style.color = '#841617';
+    artificialPaddingTwo.current.style.color = '#841617';
   }
 
   function closeModal() {
@@ -81,12 +85,17 @@ const LoggedInPopup = ()=> {
         id ="modal"
         shouldCloseOnOverlayClick={false}
       >
-        <h2 ref={heading}>Welcome Back!</h2>
-        <p ref={message}>Seems like you are already signed in. Would you like to sign out or continue to the map?</p>
-        <div ref={window} className='flex w-full h-full items-end justify-between'>
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={handleSignOut}>Sign Out</button>
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={closeModal}>Close</button>
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={handleMapRedirect}>Continue to Map</button>
+        <div ref={window} className='w-full'>
+          <h2 ref={heading}>Welcome Back!</h2>
+          <p ref={artificialPaddingTwo}>&nbsp;</p>
+          <p ref={message}>Seems like you are already signed in. Would you like to sign out or continue to the map?</p>
+          <p ref={artificialPadding}>&nbsp;</p>
+          <div className=''>
+            <div className='flex w-full justify-between'>
+              <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={handleSignOut}>Sign Out</button>
+              <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={handleMapRedirect}>Continue to Map</button>
+            </div>
+          </div>
         </div>
       </Modal>
     </div>
